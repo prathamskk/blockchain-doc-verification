062472c5a842347d8d1e965f855ce56ec5d3ec8c
Kamble Pratham Shaileshkumar Runa <prathamskk@gmail.com>
2024-04-03 23:16:52 +0530

    Update Verfication.sol

--- Contract/Verfication.sol
+++ Contract/Verfication.sol
@@ -1,109 +1,107 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity ^0.8.0;
+pragma abicoder v1;
 
 contract Verification {
-    constructor() { owner = msg.sender; }
-    uint16 public count_Exporters =0;
-    uint16 public count_hashes=0;
+    uint16 public countExporters;
+    uint16 public countHashes;
     address public owner;
 
-    struct  Record  {
-        uint blockNumber; 
-        uint minetime; 
+    struct Record {
+        uint blockNumber;
+        uint minetime;
         string info;
-        string ipfs_hash;
-         }
-    struct Exporter_Record{
+        string ipfsHash;
+    }
+
+    struct ExporterRecord {
         uint blockNumber;
         string info;
-         }
-     mapping (bytes32  => Record) private docHashes;
-     mapping (address => Exporter_Record) private Exporters;
-     
-//---------------------------------------------------------------------------------------------------------//
+    }
+
+    mapping (bytes32 => Record) private docHashes;
+    mapping (address => ExporterRecord) private exporters;
+
     modifier onlyOwner() {
-            if (msg.sender != owner) {
-            revert("Caller is not the owner"); }_; }
+        require(msg.sender == owner, "Caller is not the owner");
+        _;
+    }
 
     modifier validAddress(address _addr) {
-            assert(_addr != address(0)); _; }
+        require(_addr != address(0), "Invalid address");
+        _;
+    }
 
-   
-    modifier authorised_Exporter(bytes32  _doc){
+    modifier authorizedExporter(bytes32 _doc) {
+        require(keccak256(abi.encodePacked(exporters[msg.sender].info)) == keccak256(abi.encodePacked(docHashes[_doc].info)), "Caller is not authorized to edit this document");
+        _;
+    }
 
-         if (keccak256(abi.encodePacked((Exporters[msg.sender].info )))!= keccak256(abi.encodePacked((docHashes[_doc].info))))
-      
-        revert("Caller is not  authorised to edit this document"); 
-         _; }
+    modifier canAddHash() {
+        require(exporters[msg.sender].blockNumber != 0, "Caller not authorized to add documents");
+        _;
+    }
 
-    modifier canAddHash(){
-        require(Exporters[msg.sender].blockNumber!=0,"Caller not authorised to add documents");   _; }
+    event HashAdded(address indexed exporter, string ipfsHash);
 
-//---------------------------------------------------------------------------------------------------------//
+    constructor() {
+        owner = msg.sender;
+    }
 
-    function add_Exporter(address _add,string calldata _info) external
-    onlyOwner(){ 
-        assert(Exporters[_add].blockNumber==0);
-         
-          Exporters[_add].blockNumber = block.number;
-          Exporters[_add].info = _info;
-          ++count_Exporters;
+    function addExporter(address _addr, string calldata _info) external onlyOwner {
+        require(exporters[_addr].blockNumber == 0, "Exporter already exists");
         
-        }
+        exporters[_addr].blockNumber = block.number;
+        exporters[_addr].info = _info;
+        countExporters++;
+    }
 
-    function delete_Exporter(address _add) external  
-    onlyOwner
-    {
-        assert(Exporters[_add].blockNumber!=0);
+    function deleteExporter(address _addr) external onlyOwner {
+        require(exporters[_addr].blockNumber != 0, "Exporter does not exist");
         
-        Exporters[_add].blockNumber=0;
-        Exporters[_add].info="";
-        --count_Exporters;
-        }
-        
-    function alter_Exporter(address _add,string calldata    _newInfo) public
-    onlyOwner()
-     { 
-          assert(Exporters[_add].blockNumber!=0);
-             Exporters[_add].info=_newInfo; }
-
-    function changeOwner(address _newOwner) public 
-        onlyOwner  validAddress(_newOwner)   {  owner = _newOwner; }
-
-        event addHash(address indexed _exporter,string _ipfsHash);
-    function addDocHash (bytes32  hash,string calldata _ipfs) public 
-      canAddHash
-      {
-            assert(docHashes[hash].blockNumber==0 && docHashes[hash].minetime==0);
-            Record memory  newRecord = 
-            Record(block.number,block.timestamp,Exporters[msg.sender].info,_ipfs);
-            docHashes[hash] = newRecord; 
-            ++count_hashes;
-            emit addHash(msg.sender,_ipfs);
-      }
-      
-
-    function findDocHash (bytes32 _hash) 
-    external  view  returns (uint,uint,string memory,string memory) {
-       
-        return (docHashes[_hash].blockNumber,docHashes[_hash].minetime,docHashes[_hash].info,docHashes[_hash].ipfs_hash );
-        }
-
-    function deleteHash (bytes32 _hash) public
-    authorised_Exporter(_hash)
-    canAddHash
-    {
-    assert(docHashes[_hash].minetime!=0);
-    docHashes[_hash].blockNumber=0;
-    docHashes[_hash].minetime=0;
-    
-    --count_hashes;
-    
+        delete exporters[_addr];
+        countExporters--;
     }
     
-    function  getExporterInfo(address _add) external view returns(string memory){
+    function alterExporter(address _addr, string calldata _newInfo) external onlyOwner { 
+        require(exporters[_addr].blockNumber != 0, "Exporter does not exist");
+        
+        exporters[_addr].info = _newInfo;
+    }
 
-        return (Exporters[_add].info);
+    function changeOwner(address _newOwner) external onlyOwner validAddress(_newOwner) {  
+        owner = _newOwner;
+    }
+
+    function addDocHash(bytes32 _hash, string calldata _ipfs) external canAddHash {
+        require(docHashes[_hash].blockNumber == 0 && docHashes[_hash].minetime == 0, "Document hash already exists");
+
+        Record memory newRecord = Record({
+            blockNumber: block.number,
+            minetime: block.timestamp,
+            info: exporters[msg.sender].info,
+            ipfsHash: _ipfs
+        });
+
+        docHashes[_hash] = newRecord; 
+        countHashes++;
+        emit HashAdded(msg.sender, _ipfs);
+    }
+
+    function findDocHash(bytes32 _hash) external view returns (uint, uint, string memory, string memory) {
+        Record memory doc = docHashes[_hash];
+        return (doc.blockNumber, doc.minetime, doc.info, doc.ipfsHash);
+    }
+
+    function deleteHash(bytes32 _hash) external authorizedExporter(_hash) canAddHash {
+        require(docHashes[_hash].minetime != 0, "Document hash does not exist");
+
+        delete docHashes[_hash];
+        countHashes--;
+    }
+    
+    function getExporterInfo(address _addr) external view returns (string memory) {
+        return exporters[_addr].info;
     }
 }
